dbff1a3bb834eadd7d2a95e757e643c42a39fd5f
Vikram Hosamane <vikram.hosamane@gmail.com>
2026-08-03 15:30:34 +0530

    configation for Fonts, Images, Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 function WelcomeMessage() {
-  return <h1 className="text-3xl text-white font-black">Welcome to the world of Next js</h1>;
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-950 text-white">
+      <div className="text-center">
+        <h1 className="font-space-grotesk text-4xl font-black">Welcome to the world of Next js</h1>
+        <p className="mt-4 text-xl font-semibold">Welcome Another Font</p>
+      </div>
+    </div>
+  );
 }
 
 const Home = () => {
@@ -7,8 +14,7 @@ const Home = () => {
     <div>
       <WelcomeMessage />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
-
